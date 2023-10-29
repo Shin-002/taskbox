0f96cef5a30698b2b67b32772f9f013f795e5742
Nick <shinichiro.yukawa@shelfy.jp>
2023-10-29 23:44:29 +0900

    GitHub action setup

--- src/components/InboxScreen.stories.jsx
+++ src/components/InboxScreen.stories.jsx
@@ -1,8 +1,11 @@
 import InboxScreen from './InboxScreen';
+
 import store from '../lib/stores.js';
 import {rest} from 'msw';
 import {MockedState} from './TaskList.stories';
 import {Provider} from 'react-redux';
+
+import {fireEvent, waitFor, waitForElementToBeRemoved, within} from '@storybook/testing-library';
 
 export default {
   component: InboxScreen,
@@ -23,6 +26,18 @@ export const Default = {
         ),
       ],
     },
+  },
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+    // Waits for the component to transition from the loading state
+    await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+    // Waits for the component to be updated based on the store
+    await waitFor(async () => {
+      // Simulates pinning the first task
+      await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+      // Simulates pinning the third task
+      await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+    });
   },
 };
 export const Error = {
